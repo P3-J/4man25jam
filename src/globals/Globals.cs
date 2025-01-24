d00469bf8c9c8ee1cfc2d7ff213d04d9ffce33b8
RasmusKard <rasmuskard@gmail.com>
2025-01-24 23:40:47 +0200

    add: initial background scene

--- src/globals/Globals.cs
+++ src/globals/Globals.cs
@@ -2,11 +2,10 @@ using Godot;
 
 public partial class Globals : Node
 {
-
     public int exampleGlobal = 0;
 
-    public void GlobalPrint(){
+    public void GlobalPrint()
+    {
         GD.Print(exampleGlobal);
     }
-
 }
