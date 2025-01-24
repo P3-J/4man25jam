2c55a7da059982ecf83e76a7f4ee93d0c340d39c
RasmusKard <rasmuskard@gmail.com>
2025-01-25 00:35:19 +0200

    add: player height and levelup

--- src/globals/Globals.cs
+++ src/globals/Globals.cs
@@ -1,11 +1,45 @@
+using System.Collections.Generic;
 using Godot;
+using Godot.NativeInterop;
 
 public partial class Globals : Node
 {
+    private SignalBus sgbus;
     public int exampleGlobal = 0;
+
+    public int playerHeight = 0;
+
+    public int currentLevel = 0;
+
+    readonly List<int> LevelHeightNeededArr = new() { 0, 100, 250, 500, 750 };
 
     public void GlobalPrint()
     {
         GD.Print(exampleGlobal);
+    }
+
+    public override void _Ready()
+    {
+        sgbus = GetNode<SignalBus>("/root/Signalbus");
+    }
+
+    public void AddPlayerHeight(int height)
+    {
+        playerHeight += height;
+
+        // win game
+        if (currentLevel == LevelHeightNeededArr.Count)
+        {
+            return;
+        }
+
+        // check if height hit new level
+        int nextLevelHeightNeeded = LevelHeightNeededArr[currentLevel + 1];
+        if (playerHeight >= nextLevelHeightNeeded)
+        {
+            currentLevel++;
+            sgbus.EmitSignal("LevelUpSignal");
+        }
+        // if yes then signalbus event new level
     }
 }
--- src/globals/SignalBus.cs
+++ src/globals/SignalBus.cs
@@ -1,11 +1,11 @@
 using System;
 using Godot;
 
-
 public partial class SignalBus : Node
 {
+    [Signal]
+    public delegate void ExampleSignalEventHandler();
 
-    [Signal] public delegate void ExampleSignalEventHandler();
-
-
+    [Signal]
+    public delegate void LevelUpSignalEventHandler();
 }
