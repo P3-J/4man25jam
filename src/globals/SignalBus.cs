24297b28f2a937eabf4b2e278fc9c1e5f7c2204f
Jako Keskküla <jako.keskkyla@gmail.com>
2025-01-25 16:20:39 +0200

    lmao

--- src/globals/SignalBus.cs
+++ src/globals/SignalBus.cs
@@ -5,7 +5,10 @@ public partial class SignalBus : Node
 {
     [Signal]
     public delegate void ExampleSignalEventHandler();
-
     [Signal]
     public delegate void LevelUpSignalEventHandler();
+    [Signal]
+    public delegate void PlayerGetHitEventHandler();
+    [Signal]
+    public delegate void EnemyGetHitEventHandler();
 }
